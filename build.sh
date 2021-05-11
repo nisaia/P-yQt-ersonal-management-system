c6f58965c0ca9c37d2402cf796b5ad62365ae7fd
Nicolò <isanic6@gmail.com>
2021-05-11 15:48:26 +0200

    Updates

--- build.sh
+++ build.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 #CREATE VIRTUAL ENVIROMENT
-echo "Creating virtual enviroment"
+echo "Creating virtual enviroment..."
+sleep 3
 python3 -m venv virtual_enviroment
+
+#ACCESS INTO VIRTUAL ENVIROMENT
+echo "Access into virtual enviroment..."
+sleep 3
+. virtual_enviroment/bin/activate
+
 
 #INSTALL DEPENDENCIES
 
-echo "Installing dependencies"
+echo "Installing dependencies..."
 sleep 3
 pip install -r requirements.txt
 
 #CREATE DATABASE ENGINE
 
-echo "Creating database engine"
+echo "Creating database engine..."
 sleep 3
 python3 -c "from database.db import create_database; create_database()"
